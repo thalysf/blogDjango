5272bac803e07318a42730b1e87f43ae4e47a45f
thalysf <thalysfcandido@gmail.com>
2021-02-21 10:08:08 -0300

    Configurado o template do blog

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+
 # Registra modulos para aparecer no admin 
+
+# Modificando layout de exibição dos posts em /admin
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'sub_title', 'full_name', 'deleted'] # listagem de posts
+    search_fields = ['title', 'sub_title'] # tornando os elementos buscáveis
+
+    def get_queryset(self, request):
+        return Post.objects.filter(deleted=False)
+admin.site.register(Post, PostAdmin)
--- blog/website/models.py
+++ blog/website/models.py
@@ -2,7 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+class Categorias(models.TextChoices):
+    TECH = 'TC', 'Tecnologia'
+    CR = 'CR', 'Curiosidades'
+    GR = 'GR', 'Geral'
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     content = models.TextField(default='')
+    categories = models.CharField(
+        max_length=2,
+        choices=Categorias.choices,
+        default=Categorias.GR,
+        )
+    deleted = models.BooleanField(default=False)
+    # Exibe titulo do post na edição
+    def __str__(self):
+        return self.title
+
+    def full_name(self):
+        return self.title + self.sub_title
+
+    full_name.admin_order_field = 'title' #tornando ordenável full_name
+
+    
